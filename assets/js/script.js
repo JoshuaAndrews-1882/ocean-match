7e0e0dd89912a1c445d9b9f113ae808d72ef4672
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2021-07-12 03:32:04 +0000

    Remove code as not happy with it

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,7 @@ function runGame() {
 
 
 //flip card function
-const cards = document.querySelectorAll('.card-area');
 
-let hasFlippedCard = false;
-let firstCard, secondCard;
-
-function flipCard() {
-    this.classList.toggle('flip');
-
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-
-        console.log(firstCard.dataset.match);
-    }
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard))
 
 
 //total turns
