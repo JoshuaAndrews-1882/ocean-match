ff32c589985e977c78fe105b32d10ab38489ef6a
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2021-07-12 02:46:42 +0000

    Layout functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 const cards = document.querySelectorAll('.card-area');
 
+
+//run game
+
+
+//flip card function
 function flipCard() {
     this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
+
+//check card
+
+
+
+//total turns
+
+
+
+//time
+
+
+
+//result
