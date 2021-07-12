d259800cbabf30a0d24891c8a5ff33bc3928f003
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2021-07-12 03:19:32 +0000

    Start flipped card matching code and assigned a dataset attribute to each image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,49 @@
-const cards = document.querySelectorAll('.card-area');
-
-
 //run game
+function runGame() {
+}
+
+
+//shuffle card
 
 
 //flip card function
+const cards = document.querySelectorAll('.card-area');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log(firstCard.dataset.match);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
 
-//check card
-
-
 
 //total turns
+function totalTurns() {
+
+}
 
 
 
 //time
+function timeRemainig() {
+
+}
 
 
 
 //result
+function winOrLose() {
+
+}
